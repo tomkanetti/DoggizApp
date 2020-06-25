ef56f41b9461ffd57a12b59e50ba20c9b6659ec6
Noa Feldberg <noa7feld@gmail.com>
2020-06-25 18:42:54 +0300

    add users list view

---
+++ app/src/main/java/com/example/myapplication/Activities/FriendsListActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FriendsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_list);
+    }
+}
