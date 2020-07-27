d920b434f51187f6b0401b55e0ae96da8f66a778
TomKanetti <kanettitom@gmail.com>
2020-07-27 20:49:11 +0300

    share post image bitmap

--- app/src/main/java/com/example/myapplication/fragments/Drawer/feed/FeedFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/Drawer/feed/FeedFragment.java
@@ -1,10 +1,15 @@
 package com.example.myapplication.fragments.Drawer.feed;
 
 import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -16,9 +21,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.LiveData;
+
 
 import com.example.myapplication.R;
 import com.example.myapplication.model.User;
@@ -26,7 +33,14 @@ import com.example.myapplication.model.UserModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+
 public class FeedFragment extends DialogFragment {
+    private static final int PICK_IMAGE = 100;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private FeedViewModel feedViewModel;
     View view;
@@ -37,7 +51,7 @@ public class FeedFragment extends DialogFragment {
     Button popUpShareBtn;
     TextView popupTitle,popupDescription;
     User user;
-    private Uri pickedImgUri = null;
+    private Bitmap pickedImgBit = null;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -91,7 +105,12 @@ public class FeedFragment extends DialogFragment {
                 }
             }
         });
-
+        popupAddImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
 
 
 
@@ -112,5 +131,44 @@ public class FeedFragment extends DialogFragment {
     @SuppressWarnings("StatementWithEmptyBody")
     public boolean onNavigationItemSelected(MenuItem item) {
         return true;
+    }
+
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            Uri uri=data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+                pickedImgBit =rotateImage((Bitmap) bitmap);
+                popupPostImage.setImageBitmap(pickedImgBit);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE && data!=null) {
+//            Bundle extras= data.getExtras();
+//            pickedImgBit = rotateImage((Bitmap) extras.get("data"));
+//            popupPostImage.setImageBitmap(pickedImgBit);}
+//        }
+
+
+    public static Bitmap rotateImage(Bitmap source) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(0);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
     }
 }
